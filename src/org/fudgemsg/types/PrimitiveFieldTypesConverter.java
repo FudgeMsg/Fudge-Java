3d878dc2e086c6b28d5999414e7d8610fb8f84cd
Stephen Colebourne <stephen@opengamma.com>
2011-02-17 17:35:07 +0000

    Change copyright date

--- src/org/fudgemsg/types/PrimitiveFieldTypesConverter.java
+++ src/org/fudgemsg/types/PrimitiveFieldTypesConverter.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (C) 2009 - 2009 by OpenGamma Inc. and other contributors.
+ * Copyright (C) 2009 - present by OpenGamma Inc. and other contributors.
  * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
